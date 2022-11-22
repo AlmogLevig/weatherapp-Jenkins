4d4e0fec4dfb2ca78ba011501581535e00cceca7
almog.levi <almog.levig@gmail.com>
2022-11-22 09:42:33 +0200

    Merge branch 'feature-python' into develop

--- Python/utils.py
+++ Python/utils.py
@@ -20,15 +20,20 @@ def get_relevant_bg():
 api = {
     "key": "f738629a362bfb615e811eeeda4f40a1",
     "baseurl": "https://api.openweathermap.org/data/2.5/",
-    "city":"phuket"
+    "city":"Tel Aviv"
     }
 
+# API Request Function
+# Request APi call
+# Respond Json file of DB of weather of specific city
 def get_daily_data():
     data = f"{api['baseurl']}/weather?q={api['city']}&units=metric&APPID={api['key']}"    
     response = requests.get(data)
     return response.json()
         
 
+# Filtering Function
+# Giving desired specefic data a week ahead
 def get_weekly_data():
     city = get_daily_data()
     days = f"{api['baseurl']}onecall?lat={city['coord']['lat']}&lon={city['coord']['lon']}&units=metric&appid={api['key']}"
